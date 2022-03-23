54933f696a1bc8b1a60b1967439db141ba69aefa
Anna-2804 <82101294+Anna-2804@users.noreply.github.com>
2022-03-23 16:27:02 +0200

    Add files via upload

---
+++ index21.js
@@ -0,0 +1,22 @@
+let user = {
+    name: "John";
+    surname: "Smith"};
+    user.name = "Pete";
+    delete.user.name;
+
+//Task #2
+//1 yes
+const user = {
+    name: "John"
+}
+//2.yes
+user.name = "Pete"
+
+//Task #3
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+}
+let sum = salaries.John + salaries.Ann + salaries.Pete;
+console.log(sum)
