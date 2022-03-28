e99fe058e76db3a0672845a595e710a1f107260a
Anna-2804 <82101294+Anna-2804@users.noreply.github.com>
2022-03-28 14:57:28 +0300

    Add files via upload

---
+++ hm8/index31.js
@@ -0,0 +1,23 @@
+//Task #1
+Boolean(Number(’10’)) + 1; //2
+‘sub ’ + Number(false); //sub0
+16  *  ‘      91    ‘ ;//0
+true-70 // 69
+Number(1 + String(1)) + 1 //12
+
+//Task# 2
+let sec = 60;
+let hours = 60;
+alert(sec * hours);
+let users = prompt("How many hours?", 100);
+let NumberOfHours = (sec * hours * users);
+alert(NumberOfHours);
+//Task #3
+let num = 1;
+num += 12;
+num -= 14;
+num *= 5;
+num /= 7;
+num ++;
+num —-;
+alert(num);
