54c8e9a383169b41b3b927d15df1b65e374fa6eb
Anna-2804 <82101294+Anna-2804@users.noreply.github.com>
2022-04-09 16:15:37 +0300

    Add files via upload

---
+++ hm16/index16.js
@@ -0,0 +1,14 @@
+"use strict"
+let arg1 = prompt("enter the number", '');
+let arg2 = prompt("enter the power to which this number is to be raised", '');
+function showMessage(arg1, arg2 = 1) {
+    let result = arg1;
+    if (isNaN(arg1) || isNaN(arg2)) {
+        alert("some error");
+    } else {
+        return result;
+    }
+
+}
+let result = showMessage(arg1, arg2);
+alert (result)
