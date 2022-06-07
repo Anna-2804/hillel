c5c7f4038d6ffc0f2a5bbb031f2f2f4294bfd797
Anna-2804 <82101294+Anna-2804@users.noreply.github.com>
2022-06-07 17:27:22 +0300

    Add files via upload

---
+++ hm18/index18.js
@@ -0,0 +1,12 @@
+"use strict"
+let arr = [1, 2, 3, -1, -2, -3];
+let emptyArr = [];
+function NewFan(numbers) {
+    for (let i = 0; i > arr.length; i++) {
+        if (numbers > 0) {
+            return emptyArr.push(arr)
+        }
+        return null
+    }
+}
+console.log(emptyArr)
