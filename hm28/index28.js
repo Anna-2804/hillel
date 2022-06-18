f6c08b414e669d68a356bc66607a107db8f7b29e
Anna-2804 <82101294+Anna-2804@users.noreply.github.com>
2022-06-18 17:29:23 +0300

    Add files via upload

---
+++ hm28/index28.js
@@ -0,0 +1,14 @@
+let calculator = {
+    read(){
+        this.number = +prompt("number?", );
+    },
+    sum(){
+        return this.number + this.number;
+    },
+    mul(){
+        return this.number * this.number;
+    }
+}
+calculator.read();
+alert(calculator.sum());
+alert(calculator.mul())
